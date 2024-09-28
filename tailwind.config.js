4b935405a067a1107856cff6dad89652951764cb
shiv-pd <shivanideshmukh@gmail.com>
2024-09-28 15:00:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+      },
+      screens: {
+        'xs': '480px', // Custom extra small breakpoint
+        'sm': '640px', // Default small breakpoint
+        'md': '768px', // Default medium breakpoint
+        'lg': '1024px', // Default large breakpoint
+        'xl': '1280px', // Default extra-large breakpoint
+        '2xl': '1536px', // Default 2x extra-large breakpoint
+        '3xl': '1920px', // Custom breakpoint for very large screens
+      },
+    },
+    
+    
+  },
+  plugins: [],
+}
+
